62afa25e55becf5f5585b5bd32883a032b396509
ameena876 <162981124+ameena876@users.noreply.github.com>
2024-03-11 16:16:09 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,55 @@
+''''
+Day1
+bargained characters
+controls and data structures=abc language
+strings and its formats=from c
+regular expressions=pearl
+exception handling=module3
+magic methods from=fortron
+
+python is a
+*object oriented programming language
+*dynamically typed language
+*scripting language
+*interpreted language
+*complied language
+*functional programing language
+
+flavours of python
+*cpython-written in c,most common implemention of python
+*jython-java implementation of the python programmng language which is tightly integrated with.NET.
+*brython-browser python ,runs in browsers
+*ruby python-bridge between python and ruby interpreters
+*micropython-runs in micro controllers
+'''
+
+
+
+
+#print('ameena')
+'''
+print('hello world')
+print('hii')
+'''
+import this
+print(this)
+a=78
+print(a)
+#import is a key which is used to communicated between the python files.
+import antigravitiy
+#---> print(object,sep,end,file,flush)
+#print has 5 parameters
+
+#object -->the value to be printed is object
+#sep--> seperator
+#a=8
+#b=9#c=89
+#print(a,b,c, sep='$')
+#end-->which is used to add the string at the end value,
+## it will not keep the command to stay in same line
+#eg:
+#print(str(a), b,end='%%')
+#print(str(c))
+#!---->variables
+#? variables is a name which is used to identify the value
+
